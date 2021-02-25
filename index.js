386fbd149bd27c7fa27c47e9701b982ed9657403
prahal <vprahaladhan@gmail.com>
2021-02-26 10:51:15 +1100

    final commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const add = (a , b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const increment = a => a + 1;
+
+const decrement = a => a - 1;
+
+const makeInt = n => parseInt(n, 10);
+
+const preserveDecimal = n => parseFloat(n);
